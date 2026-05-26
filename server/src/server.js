b28551c2150e7b0f466281fcde4408a66c30f1b1
champati-v <vibekanandac15@gmail.com>
2026-05-26 13:03:36 +0530

    updated allowed origins

--- server/src/server.js
+++ server/src/server.js
@@ -4,23 +4,30 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 
-
 const authRoutes = require("./routes/auth.routes");
 const bookRoutes = require("./routes/book.routes");
 
-
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://book-management-lkf8.onrender.com",
+];
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
 connectDB();
 
-
 app.get("/", (req, res) => {
   res.send("Book Management API Running...");
 });
-
 
 // Auth Routes
 app.use("/api/auth", authRoutes);
